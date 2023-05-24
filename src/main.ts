7fb6584da0309296d511eb506bfdbc7a4a416831
adgehTech@gmail.com <belaybirhanu@gmail.com>
2023-05-24 08:57:56 +0300

    swagger docs added

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 require('dotenv').config(); 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 // import { ValidationPipe } from '@nestjs/common';
 // import { useContainer } from 'class-validator';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+  .setTitle('CRUD API')
+  .setDescription('This is Crud api built for training...')
+  .setVersion('1.0')
+  .addTag('CRUD')
+  .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   // app.useGlobalPipes(new ValidationPipe(
   //   { whitelist: true, transform: true }));
